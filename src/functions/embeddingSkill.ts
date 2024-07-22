4e24148e9f78d9d76760c7f9d596df7d465edfd5
Tyler Faulkner <tylersfaulkner@gmail.com>
2024-07-21 20:59:32 -0500

    Fixed Embedding API

--- src/functions/embeddingSkill.ts
+++ src/functions/embeddingSkill.ts
@@ -1,39 +1,60 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, Exception, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import OpenAI from 'openai';
 
-export async function embeddingSkill(req: HttpRequest, context: InvocationContext): Promise<any> {
-    context.log('Processing request for OpenAI Embeddings');
-    const authorizationHeader = req.headers['authorization'];
-    const openAiApiKey = authorizationHeader.replace('Bearer ', ''); 
-
-    const values = req.body.values;
+export async function embeddingSkill(req: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     const responses = [];
+    try {
+        context.log('Processing request for OpenAI Embeddings');
+        const authorizationHeader = req.headers.get('authorization');
 
+        const openAiApiKey = authorizationHeader.replace('Bearer ', ''); 
+        context.log(req.body);
+        const body = await req.json() as any;
 
-    const openai = new OpenAI({
-        apiKey: openAiApiKey
-    });
-
-    for await (const value of values()) {
-        const { recordId, data } = value;
-        const { text, language, phraseList } = data;
-
-        const embeddingResponse = await openai.embeddings.create({
-            model: 'text-embedding-v1',
-            input: text,
+        const values = body.values;
+        context.log("Encoding " + values.length + " values");
+        const openai = new OpenAI({
+            apiKey: openAiApiKey
         });
+        for await (const value of values) {
+            const { recordId, data } = value;
+            const { text, language, phraseList } = data;
 
-        const embedding = embeddingResponse.data
+            const embeddingResponse = await openai.embeddings.create({
+                model: 'text-embedding-3-small',
+                input: text,
+            });
 
-        responses.push({
-            recordId,
-            data: { embedding },
-            errors: null,
-            warnings: null
-        });
+            const embedding = embeddingResponse.data[0].embedding;
+
+            responses.push({
+                recordId,
+                data: { embedding },
+                errors: null,
+                warnings: null
+            });
+        }
+    }
+    catch (error : any) {
+        context.error('Error processing values: ' + error);
+        return {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                error: error
+            })
+        };
     }
 
-    return { values: responses };
+    return {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ values: responses })
+    };
 };
 
 app.http('embeddingSkill', {
